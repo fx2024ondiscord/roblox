357fc563a2e4f30c258a8063e84029e8f5931fb6
Some_But!TOOL <143144389+fx2024ondiscord@users.noreply.github.com>
2024-10-27 18:46:04 -0800

    Update mobile.lua

--- SSL.[fx]/mobile/mobile.lua
+++ SSL.[fx]/mobile/mobile.lua
@@ -30,73 +30,106 @@ local Mouse = LocalPlayer:GetMouse()
 local HTTPService = game:GetService("HttpService")
 
 local Library = {
-	Themes = {
-		Legacy = {
-			Main = Color3.fromHSV(262/360, 60/255, 34/255),
-			Secondary = Color3.fromHSV(240/360, 40/255, 63/255),
-			Tertiary = Color3.fromHSV(260/360, 60/255, 148/255),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Serika = {
-			Main = Color3.fromRGB(50, 52, 55),
-			Secondary = Color3.fromRGB(80, 82, 85),
-			Tertiary = Color3.fromRGB(226, 183, 20),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Dark = {
-			Main = Color3.fromRGB(30, 30, 35),
-			Secondary = Color3.fromRGB(50, 50, 55),
-			Tertiary = Color3.fromRGB(70, 130, 180),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Rust = {
-			Main = Color3.fromRGB(37, 35, 33),
-			Secondary = Color3.fromRGB(65, 63, 63),
-			Tertiary = Color3.fromRGB(237, 94, 38),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Aqua = {
-			Main = Color3.fromRGB(19, 21, 21),
-			Secondary = Color3.fromRGB(65, 63, 63),
-			Tertiary = Color3.fromRGB(51, 153, 137),
-
-			StrongText = Color3.fromHSV(0, 0, 1),        
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Vaporwave = {},
-		OperaGX = {},
-		VisualStudio = {}
-	},
-	ColorPickerStyles = {
-		Legacy = 0,
-		Modern = 1
-	},
-	Toggled = true,
-	ThemeObjects = {
-		Main = {},
-		Secondary = {},
-		Tertiary = {},
-
-		StrongText = {},
-		WeakText = {}
-	},
-	WelcomeText = nil,
-	DisplayName = nil,
-	DragSpeed = 0.06,
-	LockDragging = false,
-	ToggleKey = Enum.KeyCode.Home,
-	UrlLabel = nil,
-	Url = nil
-
+    Themes = {
+        Legacy = {
+            Main = Color3.fromHSV(262/360, 60/255, 34/255),
+            Secondary = Color3.fromHSV(240/360, 40/255, 63/255),
+            Tertiary = Color3.fromHSV(260/360, 60/255, 148/255),
+            StrongText = Color3.fromHSV(0, 0, 1),
+            WeakText = Color3.fromHSV(0, 0, 172/255)
+        },
+        Serika = {
+            Main = Color3.fromRGB(50, 52, 55),
+            Secondary = Color3.fromRGB(80, 82, 85),
+            Tertiary = Color3.fromRGB(226, 183, 20),
+            StrongText = Color3.fromHSV(0, 0, 1),
+            WeakText = Color3.fromHSV(0, 0, 172/255)
+        },
+        Dark = {
+            Main = Color3.fromRGB(30, 30, 35),
+            Secondary = Color3.fromRGB(50, 50, 55),
+            Tertiary = Color3.fromRGB(70, 130, 180),
+            StrongText = Color3.fromHSV(0, 0, 1),
+            WeakText = Color3.fromHSV(0, 0, 172/255)
+        },
+        Rust = {
+            Main = Color3.fromRGB(37, 35, 33),
+            Secondary = Color3.fromRGB(65, 63, 63),
+            Tertiary = Color3.fromRGB(237, 94, 38),
+            StrongText = Color3.fromHSV(0, 0, 1),
+            WeakText = Color3.fromHSV(0, 0, 172/255)
+        },
+        Aqua = {
+            Main = Color3.fromRGB(19, 21, 21),
+            Secondary = Color3.fromRGB(65, 63, 63),
+            Tertiary = Color3.fromRGB(51, 153, 137),
+            StrongText = Color3.fromHSV(0, 0, 1),
+            WeakText = Color3.fromHSV(0, 0, 172/255)
+        },
+        Vaporwave = {},
+        OperaGX = {},
+        VisualStudio = {}
+    },
+    ColorPickerStyles = {
+        Legacy = 0,
+        Modern = 1
+    },
+    Toggled = true,
+    ThemeObjects = {
+        Main = {},
+        Secondary = {},
+        Tertiary = {},
+        StrongText = {},
+        WeakText = {}
+    },
+    WelcomeText = nil,
+    DisplayName = nil,
+    DragSpeed = 0.06,
+    LockDragging = false,
+    ToggleKey = Enum.KeyCode.Home,
+    UrlLabel = nil,
+    Url = nil
 }
+
+function Library:HideUI()
+    for _, object in pairs(self.ThemeObjects.Main) do
+        object.Visible = false
+    end
+    for _, object in pairs(self.ThemeObjects.Secondary) do
+        object.Visible = false
+    end
+    for _, object in pairs(self.ThemeObjects.Tertiary) do
+        object.Visible = false
+    end
+    for _, object in pairs(self.ThemeObjects.StrongText) do
+        object.Visible = false
+    end
+    for _, object in pairs(self.ThemeObjects.WeakText) do
+        object.Visible = false
+    end
+end
+
+function Library:ToggleUI()
+    self.Toggled = not self.Toggled
+    for _, object in pairs(self.ThemeObjects.Main) do
+        object.Visible = self.Toggled
+    end
+    for _, object in pairs(self.ThemeObjects.Secondary) do
+        object.Visible = self.Toggled
+    end
+    for _, object in pairs(self.ThemeObjects.Tertiary) do
+        object.Visible = self.Toggled
+    end
+    for _, object in pairs(self.ThemeObjects.StrongText) do
+        object.Visible = self.Toggled
+    end
+    for _, object in pairs(self.ThemeObjects.WeakText) do
+        object.Visible = self.Toggled
+    end
+end
+
+
+
 Library.__index = Library
 
 local selectedTab
